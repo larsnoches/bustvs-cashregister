cd5b2177751d9f6e5734484ec76e7c554d46b0fd
larsnoches <cyr.selyanin@gmail.com>
2022-06-23 22:56:30 +0500

    MessagingConfig renamed, added javadoc

--- src/main/java/org/cyrilselyanin/cashregister/config/MqConfig.java
+++ src/main/java/org/cyrilselyanin/cashregister/config/MqConfig.java
@@ -4,19 +4,40 @@ import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Message queuing configuration
+ */
 @Configuration
-public class MessagingConfig {
+public class MqConfig {
+    /**
+     * Direct exchange bean
+     * @return
+     */
     @Bean
     public DirectExchange direct() {
         return new DirectExchange("cashregister.direct");
     }
 
+    /**
+     * Configuration for queue, binding and receive instance
+     */
     private static class ReceiveConfig {
+        /**
+         * Queue bean, without strict name.
+         * Used with SPEL {autoDeletingQueue.name}
+         * @return
+         */
         @Bean
         public Queue autoDeletingQueue() {
             return new AnonymousQueue();
         }
 
+        /**
+         * Binding for regcash routing key
+         * @param directExchange
+         * @param queue
+         * @return
+         */
         @Bean
         public Binding binding(DirectExchange directExchange, Queue queue) {
             return BindingBuilder.
