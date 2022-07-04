62d221fbe50a64dd26bf63d1b70591c7c24dbaf0
Cyril <cyr.selyanin@gmail.com>
2022-07-04 22:32:55 +0500

    sbis-auth in yml, rabbitListener receive method fragment calling a sbis auth and retail services

--- src/main/java/org/cyrilselyanin/cashregister/service/CashRegisterReceiverService.java
+++ src/main/java/org/cyrilselyanin/cashregister/service/CashRegisterReceiverService.java
@@ -13,11 +13,21 @@ import org.springframework.stereotype.Service;
 @Service
 @RabbitListener(queues = "#{autoDeletingQueue.name}")
 public class CashRegisterReceiverService {
-    private final SbisServiceImpl sbisServiceImpl;
+    private final SbisServiceImpl sbisService;
     private final Logger logger = LoggerFactory.getLogger(CashRegisterReceiverService.class);
 
-    public CashRegisterReceiverService(SbisServiceImpl sbisServiceImpl) {
-        this.sbisServiceImpl = sbisServiceImpl;
+    /**
+    * Properties for token requesting
+    */
+    @Value("#{cashregister.sbis-auth.appClientId}")
+    private String appClientId;
+    @Value("#{cashregister.sbis-auth.appSecret}")
+    private String appSecret;
+    @Value("#{cashregister.sbis-auth.secretKey}")
+    private String secretKey;
+
+    public CashRegisterReceiverService(SbisServiceImpl sbisService) {
+        this.sbisService = sbisService;
     }
 
     /**
@@ -26,8 +36,42 @@ public class CashRegisterReceiverService {
      */
     @RabbitHandler
     public void receive(TicketDto in) {
-        // sbisServiceImpl.requestToken();
         logger.debug("Incoming ticket dto");
         logger.debug("{}", in);
+        logger.debug("...printed ticket dto");
+
+        // reg cash with old token props
+        Optional<String> token = Optional.of(sbisService.getToken());
+        Optional<String> sid = Optional.of(sbisService.getSid());
+        if (token.isPresent() && sid.isPresent()) {
+            try {
+                sbisService.regCash(in);
+                logger.debug("token and sid are present, regcash is called");
+            } catch (RegCashException ex) {
+                logger.error("With already set auth props regCash throws exception. {}", ex);
+            }
+            return;
+        }
+
+        // new dto for token request
+        TokenRequestDto requestDto = TokenRequestDto.create(
+                appClientId,
+                appSecret,
+                secretKey
+        );
+
+        try {
+            sbisService.requestToken(requestDto);
+            token = Optional.of(sbisService.getToken());
+            sid = Optional.of(sbisService.getSid());
+            if (token.isPresent() && sid.isPresent()) {
+                sbisService.regCash(in);
+                logger.debug("token and sid are present, regcash is called");
+            }
+        } catch (RegCashException ex) {
+            logger.error("With new auth props regCash throws exception. {}", ex);
+        } catch (IOException ex) {
+            logger.error("Trying to auth and got exception. {}", ex);
+        }
     }
 }
